b299bb23dde516c756c1a5402feda4443a1fcc1d
AbeYuhi <k022g0032@g.neec.ac.jp>
2025-01-29 15:35:23 +0900

    クリスタルに光の反射を追加

--- MyEngine/DirectXGame/Manager/LightObjectManager.cpp
+++ MyEngine/DirectXGame/Manager/LightObjectManager.cpp
@@ -52,7 +52,8 @@ namespace MyEngine {
 		for (int index = 0; index < kDirectionLightNum; index++) {
 			lightData_->directionalLight[index].type = 2;
 			lightData_->directionalLight[index].color = { 1.0f, 1.0f, 1.0f, 1.0f };
-			lightData_->directionalLight[index].direction = { 0.0f, -1.0f, 0.0f };
+			lightData_->directionalLight[index].direction = { 0.7f, -1.0f, 0.3f };
+			lightData_->directionalLight[index].direction = Normalize(lightData_->directionalLight[index].direction);
 			lightData_->directionalLight[index].intensity = 1.0f;
 		}
 
