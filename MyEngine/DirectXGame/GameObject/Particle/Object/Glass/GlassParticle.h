d4d454bb1a12ad797aa6f313c08c0598d5f1ed21
友飛 阿部 <maronn.0106.2525@gmail.com>
2025-03-28 14:22:39 +0900

    kouzoutaiwotuika

--- MyEngine/DirectXGame/GameObject/Particle/Object/Glass/GlassParticle.h
+++ MyEngine/DirectXGame/GameObject/Particle/Object/Glass/GlassParticle.h
@@ -9,6 +9,19 @@ struct Triangle {
 	Triangle(Vector3 a, Vector3 b, Vector3 c) : a(a), b(b), c(c) {}
 };
 
+struct Edge {
+    Vector2 start, end;
+    Edge(Vector2 s, Vector2 e) : start(s), end(e) {}
+
+    bool operator<(const Edge& other) const {
+        // ソートのための比較演算子（順序を考慮してエッジを一意に識別）
+        if (start.x != other.start.x) return start.x < other.start.x;
+        if (start.y != other.start.y) return start.y < other.start.y;
+        if (end.x != other.end.x) return end.x < other.end.x;
+        return end.y < other.end.y;
+    }
+};
+
 class Glass;
 
 class GlassParticle
